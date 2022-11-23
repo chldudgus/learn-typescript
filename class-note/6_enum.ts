05431e858fef50fa17fbcc9c5f1026fc216a79bf
haleychoi <chldudgus617@gmail.com>
2022-11-23 15:26:21 +0900

    문자형 이넘

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -1,3 +1,4 @@
+// 숫자형 이넘
 enum Shoes {
     Nike,
     Adidas
@@ -5,3 +6,12 @@ enum Shoes {
 
 var myShoes = Shoes.Nike;
 console.log(myShoes); // 0 출력
+
+// 문자형 이넘
+enum Shoes1 {
+  Nike = "나이키",
+  Adidas = "아디다스",
+}
+
+var myShoes1 = Shoes1.Nike;
+console.log(myShoes1);
