d27f6bc9e0bd77cc4b87831549902de6844ed10a
haleychoi <chldudgus617@gmail.com>
2022-11-23 14:35:14 +0900

    타입 별칭과 인터페이스의 차이점

--- class-note/4_type-alliases.ts
+++ class-note/4_type-alliases.ts
@@ -12,3 +12,11 @@ var haley: Person = {
     name: "haley",
     age: 25
 }
+
+type MyString = string;
+var str: MyString = 'hello';
+
+type Todo = { id: string; title: string; done: boolean };
+function getTodo(todo: Todo) {
+
+}
