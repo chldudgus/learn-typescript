e7b811dca3cd951fd786a652b8a7d297da7c8622
haleychoi <chldudgus617@gmail.com>
2022-11-23 14:14:42 +0900

    타입 별칭 코드 예제

---
+++ class-note/4_type-alliases.ts
@@ -0,0 +1,14 @@
+// interface Person {
+//     name: string;
+//     age: number;
+// }
+
+type Person = {
+    name: string;
+    age: number;
+}
+
+var haley: Person = {
+    name: "haley",
+    age: 25
+}
