a03aea954a6026c48a37981a36aef16bc85de435
haleychoi <chldudgus617@gmail.com>
2022-11-23 13:21:17 +0900

    함수의 인자를 정의하는 인터페이스

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -3,7 +3,19 @@ interface User {
     name: string;
 }
 
+// 변수에 인터페이스 활용
 var haley: User = {
     age: 25,
     name: "Haley"
 }
+
+// 함수에 인터페이스 활용
+function getUser(user: User) {
+    console.log(user);
+}
+
+const capt = {
+    age: 30,
+    name: "Captain"
+}
+getUser(capt);
