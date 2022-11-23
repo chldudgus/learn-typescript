f9b84a7ea4ab75668ae88ca17402339a2dd19011
haleychoi <chldudgus617@gmail.com>
2022-11-23 15:00:59 +0900

    유니온 타입의 장점

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -1,12 +1,18 @@
 // function logMessage(value: any) {
 //     console.log(value);
 // }
-
 // logMessage('hello');
 // logMessage(100);
 
+var haley: string | number | boolean;
 function logMessage(value: string | number) {
-    console.log(value);
+    if (typeof value == 'number') {
+        value.toLocaleString();
+    }
+    if (typeof value == 'string') {
+        value.toString();
+    }
+    throw new TypeError('value must be string or number');
 }
 logMessage('hello');
 logMessage(100);
