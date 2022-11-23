4ab1ea4a01bc1f64752d219d16d3376ab7db7245
haleychoi <chldudgus617@gmail.com>
2022-11-23 13:53:42 +0900

    인터페이스 딕셔너리 패턴

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -36,4 +36,21 @@ interface StringArray {
 }
 
 var arr: StringArray = ['a', 'b', 'c'];
-arr[0] = 10; // 'a'
+// arr[0] = 10; // 'a'
+
+// 딕셔너리 패턴
+interface StringRegexDictionary {
+    [key: string]: RegExp
+}
+
+var obj: StringRegexDictionary = {
+    // sth: /abc/
+    cssFile: /\.css$/,
+    jsFile: /\.js$/,
+}
+
+// obj['cssFile'] = 'a'
+
+Object.keys(obj).forEach(function(value) {
+
+})
